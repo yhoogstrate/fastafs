6d37a22c6e3c0a51abfb5cec03887ae1c5a07c3e
yhoogstrate <y.hoogstrate@gmail.com>
2017-11-14 21:34:04 +0100

    more init

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 
 #include <iostream>
+#include <array>
+#include <vector>
+
+
+
+
+// https://github.com/facebook/zstd/issues/521
+
+struct fasta_index {
+	std::vector<std::string> sequences;
+};
+
+struct fasta {
+	bool compressed;
+	bool twobit;
+
+	fasta_index index;
+};
+
+class Fasta {
+	int cache(void) {
+		return 0;
+	}
+	
+	
+};
 
 int main()
 {
-	std::cout << "Hello World!";
+	std::cout << "usage: fastafs index -n hg38 test.fa";
 	return 0;
 }
 
