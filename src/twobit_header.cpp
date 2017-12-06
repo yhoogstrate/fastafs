71c6981e9fe706779359697d7bc1ffffa29d7fb7
yhoogstrate <y.hoogstrate@gmail.com>
2017-12-06 17:16:21 +0100

    asdads

--- src/twobit_header.cpp
+++ src/twobit_header.cpp
@@ -8,5 +8,40 @@
 
 void twobit_header::load(std::string filename) {
     std::cout << "parsing file: " << filename << std::endl;
+    
+    // open and read first four bytes
+    
+    
+    std::streampos size;
+    char * memblock;
+
+    std::ifstream file (filename, std::ios::in|std::ios::binary|std::ios::ate);
+    if (file.is_open())
+    {
+        size = file.tellg();
+        std::cout << size << " bytes\n";
+        if(size < 16) {
+            file.close();
+            throw std::invalid_argument("Corrupt file: " + filename);
+        }
+        else {
+            // magic
+            // version
+            // 
+            memblock = new char [17];
+            file.seekg (0, std::ios::beg);
+            file.read (memblock, 16);
+            memblock[16] = '\0';
+            printf("[%s]\n", memblock);
+            
+            file.close();
+
+            std::cout << "the entire file content is in memory";
+
+            delete[] memblock;
+        }
+    }
+    
+    else std::cout << "Unable to open file";
 }
 
