6fbb12ad1af2199013885f53c3663e428a45118b
yhoogstrate <y.hoogstrate@gmail.com>
2017-12-13 15:00:42 +0100

    tests and simlinks?

--- test/view/test_view.cpp
+++ test/view/test_view.cpp
@@ -20,7 +20,7 @@ BOOST_AUTO_TEST_CASE(test_chunked_viewing)
 {
     std::string fastafs_file = "tmp/test.fastafs";
     
-    fasta_to_fastafs f = fasta_to_fastafs("test", "test/view/test.fa");
+    fasta_to_fastafs f = fasta_to_fastafs("test", "test/cache/test.fa");
     f.cache();
     f.write(fastafs_file);
     
