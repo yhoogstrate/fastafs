bd61aec6702973bd4b9cdaf3d25dffecd0aae367
yhoogstrate <y.hoogstrate@gmail.com>
2017-12-18 16:21:29 +0100

    testing padding 5

--- test/view/test_view.cpp
+++ test/view/test_view.cpp
@@ -22,7 +22,7 @@ seq1:
  *                     0123...16
 
 
- * @test
+ * @test just padding and buf size = 100, no offsets
  */
 BOOST_AUTO_TEST_CASE(test_chunked_viewing)
 {
@@ -38,38 +38,45 @@ BOOST_AUTO_TEST_CASE(test_chunked_viewing)
     fastafs fs = fastafs("test");
     fs.load(fastafs_file);
     
-    char *buffer = new char[100];
+    char *buffer = new char[100];// buffer needs to be c buffer because of the fuse layer
+    std::string std_buffer;
     
     
+    // test 1: padding: 4
     written = fs.view_fasta_chunk(4, buffer, 100, 0);
     BOOST_CHECK_EQUAL(written, 100);
     
-    std::string std_buffer = std::string(buffer);
+    std_buffer = std::string(buffer);
     
     //>chr1 TTTT CCCC AAAA GGGG >chr2 ACTG ACTG NNNN ACTG >chr3.1 ACTG ACTG AAAA C >chr3.2 ACTG ACTG AAAA CC >chr3.3 ACTGACTGAAAACCC >chr4 ACTGNNNN >chr5 NNACTG 
     //----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|
-    
     BOOST_CHECK_EQUAL(std_buffer.compare(">chr1\nTTTT\nCCCC\nAAAA\nGGGG\n>chr2\nACTG\nACTG\nNNNN\nACTG\n>chr3.1\nACTG\nACTG\nAAAA\nC\n>chr3.2\nACTG\nACTG\nAAAA\n"), 0);
 
 
+    // test 2: padding: 999 - longer than longest seq
+    written = fs.view_fasta_chunk(999, buffer, 100, 0);
+    BOOST_CHECK_EQUAL(written, 100);
+    
+    std_buffer = std::string(buffer);
+    
+    //>chr1 TTTTCCCCAAAAGGGG >chr2 ACTGACTGNNNNACTG >chr3.1 ACTGACTGAAAAC >chr3.2 ACTGACTGAAAACC >chr3.3 ACTGACTGAAAACCC >chr4 ACTGNNNN >chr5 NNACTG 
+    //----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|
+    BOOST_CHECK_EQUAL(std_buffer.compare(">chr1\nTTTTCCCCAAAAGGGG\n>chr2\nACTGACTGNNNNACTG\n>chr3.1\nACTGACTGAAAAC\n>chr3.2\nACTGACTGAAAACC\n>chr3.3\nA"), 0);
+
+
+    // test 3: padding: 5 - longer than longest seq
+    written = fs.view_fasta_chunk(5, buffer, 100, 0);
+    BOOST_CHECK_EQUAL(written, 100);
+    
+    std_buffer = std::string(buffer);
+    
+    //>chr1 TTTTC CCCAA AAGGG G >chr2 ACTGA CTGNN NNACT G >chr3.1 ACTGA CTGAA AAC >chr3.2 ACTGA CTGAA AACC >chr3.3 ACTGA CTGAA AACCC >chr4 ACTGN NNN >chr5 NNACT G 
+    //----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|
+    BOOST_CHECK_EQUAL(std_buffer.compare(">chr1\nTTTTC\nCCCAA\nAAGGG\nG\n>chr2\nACTGA\nCTGNN\nNNACT\nG\n>chr3.1\nACTGA\nCTGAA\nAAC\n>chr3.2\nACTGA\nCTGAA\nAACC"), 0);
+
+
 
     
-    /*
-    
-    
-    for(unsigned int i = 0 ; i < 100 ; i++) {
-        if(buffer[i] == 123) {
-            buffer[i] = '?';
-        }
-        printf("[%c] ",buffer[i]);
-        
-        if((i+1) % 10 == 0) {
-            std::cout << "\n";
-        }
-    }
-    */
-    printf("%s\n", buffer);
-
     delete buffer;
 }
 
