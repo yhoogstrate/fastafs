8295f1b5aa6686e741eb4de1e758d2fa3f1c8b4b
yhoogstrate <y.hoogstrate@gmail.com>
2017-12-18 17:04:44 +0100

    full testing back enabled

--- test/view/test_view.cpp
+++ test/view/test_view.cpp
@@ -33,37 +33,37 @@ BOOST_AUTO_TEST_CASE(test_chunked_viewing)
     std::string std_buffer;
     
     
-    //// test 1: padding: 4
-    //written = fs.view_fasta_chunk(4, buffer, 100, 0);
-    //BOOST_CHECK_EQUAL(written, 100);
+    // test 1: padding: 4
+    written = fs.view_fasta_chunk(4, buffer, 100, 0);
+    BOOST_CHECK_EQUAL(written, 100);
     
-    //std_buffer = std::string(buffer);
+    std_buffer = std::string(buffer);
     
-    ////>chr1 TTTT CCCC AAAA GGGG >chr2 ACTG ACTG NNNN ACTG >chr3.1 ACTG ACTG AAAA C >chr3.2 ACTG ACTG AAAA CC >chr3.3 ACTGACTGAAAACCC >chr4 ACTGNNNN >chr5 NNACTG 
-    ////----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|
-    //BOOST_CHECK_EQUAL(std_buffer.compare(">chr1\nTTTT\nCCCC\nAAAA\nGGGG\n>chr2\nACTG\nACTG\nNNNN\nACTG\n>chr3.1\nACTG\nACTG\nAAAA\nC\n>chr3.2\nACTG\nACTG\nAAAA\n"), 0);
+    //>chr1 TTTT CCCC AAAA GGGG >chr2 ACTG ACTG NNNN ACTG >chr3.1 ACTG ACTG AAAA C >chr3.2 ACTG ACTG AAAA CC >chr3.3 ACTGACTGAAAACCC >chr4 ACTGNNNN >chr5 NNACTG 
+    //----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|
+    BOOST_CHECK_EQUAL(std_buffer.compare(">chr1\nTTTT\nCCCC\nAAAA\nGGGG\n>chr2\nACTG\nACTG\nNNNN\nACTG\n>chr3.1\nACTG\nACTG\nAAAA\nC\n>chr3.2\nACTG\nACTG\nAAAA\n"), 0);
 
 
-    //// test 2: padding: 999 - longer than longest seq
-    //written = fs.view_fasta_chunk(999, buffer, 100, 0);
-    //BOOST_CHECK_EQUAL(written, 100);
+    // test 2: padding: 999 - longer than longest seq
+    written = fs.view_fasta_chunk(999, buffer, 100, 0);
+    BOOST_CHECK_EQUAL(written, 100);
     
-    //std_buffer = std::string(buffer);
+    std_buffer = std::string(buffer);
     
-    ////>chr1 TTTTCCCCAAAAGGGG >chr2 ACTGACTGNNNNACTG >chr3.1 ACTGACTGAAAAC >chr3.2 ACTGACTGAAAACC >chr3.3 ACTGACTGAAAACCC >chr4 ACTGNNNN >chr5 NNACTG 
-    ////----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|
-    //BOOST_CHECK_EQUAL(std_buffer.compare(">chr1\nTTTTCCCCAAAAGGGG\n>chr2\nACTGACTGNNNNACTG\n>chr3.1\nACTGACTGAAAAC\n>chr3.2\nACTGACTGAAAACC\n>chr3.3\nA"), 0);
+    //>chr1 TTTTCCCCAAAAGGGG >chr2 ACTGACTGNNNNACTG >chr3.1 ACTGACTGAAAAC >chr3.2 ACTGACTGAAAACC >chr3.3 ACTGACTGAAAACCC >chr4 ACTGNNNN >chr5 NNACTG 
+    //----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|
+    BOOST_CHECK_EQUAL(std_buffer.compare(">chr1\nTTTTCCCCAAAAGGGG\n>chr2\nACTGACTGNNNNACTG\n>chr3.1\nACTGACTGAAAAC\n>chr3.2\nACTGACTGAAAACC\n>chr3.3\nA"), 0);
 
 
-    //// test 3: padding: 5 - see if 2bit works
-    //written = fs.view_fasta_chunk(5, buffer, 100, 0);
-    //BOOST_CHECK_EQUAL(written, 100);
+    // test 3: padding: 5 - see if 2bit works
+    written = fs.view_fasta_chunk(5, buffer, 100, 0);
+    BOOST_CHECK_EQUAL(written, 100);
     
-    //std_buffer = std::string(buffer);
+    std_buffer = std::string(buffer);
     
-    ////>chr1 TTTTC CCCAA AAGGG G >chr2 ACTGA CTGNN NNACT G >chr3.1 ACTGA CTGAA AAC >chr3.2 ACTGA CTGAA AACC >chr3.3 ACTGA CTGAA AACCC >chr4 ACTGN NNN >chr5 NNACT G 
-    ////----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|
-    //BOOST_CHECK_EQUAL(std_buffer.compare(">chr1\nTTTTC\nCCCAA\nAAGGG\nG\n>chr2\nACTGA\nCTGNN\nNNACT\nG\n>chr3.1\nACTGA\nCTGAA\nAAC\n>chr3.2\nACTGA\nCTGAA\nAACC"), 0);
+    //>chr1 TTTTC CCCAA AAGGG G >chr2 ACTGA CTGNN NNACT G >chr3.1 ACTGA CTGAA AAC >chr3.2 ACTGA CTGAA AACC >chr3.3 ACTGA CTGAA AACCC >chr4 ACTGN NNN >chr5 NNACT G 
+    //----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|----.----|
+    BOOST_CHECK_EQUAL(std_buffer.compare(">chr1\nTTTTC\nCCCAA\nAAGGG\nG\n>chr2\nACTGA\nCTGNN\nNNACT\nG\n>chr3.1\nACTGA\nCTGAA\nAAC\n>chr3.2\nACTGA\nCTGAA\nAACC"), 0);
 
 
     // test 4: padding: 1
