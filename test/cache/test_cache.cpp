9937f56595c03267fc884c2548671fb05d563ba0
yhoogstrate <y.hoogstrate@gmail.com>
2017-12-27 10:41:14 +0100

    valgrind fixes

--- test/cache/test_cache.cpp
+++ test/cache/test_cache.cpp
@@ -40,10 +40,10 @@ BOOST_AUTO_TEST_CASE(test_equality_twobit_byte)
 	BOOST_CHECK_EQUAL(strcmp(seq3, "TTT"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq4, "TTTT"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq, "TTTT"), 0);
-	delete seq1;
-	delete seq2;
-	delete seq3;
-	delete seq4;
+	delete[] seq1;
+	delete[] seq2;
+	delete[] seq3;
+	delete[] seq4;
 	
 	
 	// test 00 00 11 00 -> 00001100 -> 8+4 -> 12
@@ -62,10 +62,10 @@ BOOST_AUTO_TEST_CASE(test_equality_twobit_byte)
 	BOOST_CHECK_EQUAL(strcmp(seq3, "TTG"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq4, "TTGT"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq, "TTGT"), 0);
-	delete seq1;
-	delete seq2;
-	delete seq3;
-	delete seq4;
+	delete[] seq1;
+	delete[] seq2;
+	delete[] seq3;
+	delete[] seq4;
 	
 	
 	// test 00 11 00 00 -> 00110000 -> 16+32 -> 48
@@ -84,10 +84,10 @@ BOOST_AUTO_TEST_CASE(test_equality_twobit_byte)
 	BOOST_CHECK_EQUAL(strcmp(seq3, "TGT"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq4, "TGTT"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq, "TGTT"), 0);
-	delete seq1;
-	delete seq2;
-	delete seq3;
-	delete seq4;
+	delete[] seq1;
+	delete[] seq2;
+	delete[] seq3;
+	delete[] seq4;
 	
 	
 	// test 11 00 00 00 -> 11000000 -> 64+128 -> 192
@@ -106,10 +106,10 @@ BOOST_AUTO_TEST_CASE(test_equality_twobit_byte)
 	BOOST_CHECK_EQUAL(strcmp(seq3, "GTT"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq4, "GTTT"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq, "GTTT"), 0);
-	delete seq1;
-	delete seq2;
-	delete seq3;
-	delete seq4;
+	delete[] seq1;
+	delete[] seq2;
+	delete[] seq3;
+	delete[] seq4;
 	
 	
 	// test 10 01 01 10 -> 10010110 -> 2 + 4 + 16 + 128 -> 150
@@ -128,10 +128,10 @@ BOOST_AUTO_TEST_CASE(test_equality_twobit_byte)
 	BOOST_CHECK_EQUAL(strcmp(seq3, "ACC"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq4, "ACCA"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq, "ACCA"), 0);
-	delete seq1;
-	delete seq2;
-	delete seq3;
-	delete seq4;
+	delete[] seq1;
+	delete[] seq2;
+	delete[] seq3;
+	delete[] seq4;
 	
 	
 	
@@ -151,10 +151,10 @@ BOOST_AUTO_TEST_CASE(test_equality_twobit_byte)
 	BOOST_CHECK_EQUAL(strcmp(seq3, "GGG"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq4, "GGGG"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq, "GGGG"), 0);
-	delete seq1;
-	delete seq2;
-	delete seq3;
-	delete seq4;
+	delete[] seq1;
+	delete[] seq2;
+	delete[] seq3;
+	delete[] seq4;
 	
 	// test 00 00 00 00 -> 00000000 -> 0
 	b.set(6, NUCLEOTIDE_T);
@@ -172,10 +172,10 @@ BOOST_AUTO_TEST_CASE(test_equality_twobit_byte)
 	BOOST_CHECK_EQUAL(strcmp(seq3, "TTT"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq4, "TTTT"), 0);
 	BOOST_CHECK_EQUAL(strcmp(seq, "TTTT"), 0);
-	delete seq1;
-	delete seq2;
-	delete seq3;
-	delete seq4;
+	delete[] seq1;
+	delete[] seq2;
+	delete[] seq3;
+	delete[] seq4;
 }
 
 
