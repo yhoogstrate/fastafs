15ed06d0529e0c3f443a46131188ed01b882e51a
yhoogstrate <y.hoogstrate@gmail.com>
2018-01-19 16:27:24 +0100

    std::swap <3

--- test/tree/test_tree.cpp
+++ test/tree/test_tree.cpp
@@ -17,25 +17,49 @@ BOOST_AUTO_TEST_CASE(test_finding_long_subseqs)
     int min_subseq_len = 12;
     
     std::string shortest = "ATCGATCGATCGATCGACGTCGTCGACTAGCTAGCTAGCTAGCTAGTCGCTA";
-    std::string longest  = "GCGCGCGCGATTATCGACGTCGTCGACTAGCTAGCGGCTTTCGTAAACCCAATCTTTTT";
+    std::string longest  = "ACGCGCGCGATTATCGACGTCGTCGACTAGCTAGCGGCTTTCGTAAACCCAATCTTTTT";
     
-    std::cout << "    " << shortest << "\n";
+    std::cout << "     " << shortest << "\n";
     
-    unsigned int size_s = shortest.size();
-    unsigned int size_l = longest.size();
+    const unsigned int size_s = shortest.size();
+    const unsigned int size_l = longest.size();
+
+    std::vector<unsigned int> trace1(size_s, 0);
+    std::vector<unsigned int> trace2(size_s, 0);
+    
+    std::vector<unsigned int> *previous = &trace1;
+    std::vector<unsigned int> *current = &trace2;
     
     unsigned int l, s;
     for(l = 0 ; l < size_l; l++) {
-        std::cout << longest[l] << "   ";
+        std::cout << longest[l] << "  | ";
+        for(s = 0; s < size_s; s++) {
+            printf("%-2u", previous->at(s));
+        }
+        std::cout << "\n   | ";
         for(s = 0; s < size_s; s++) {
             if(longest[l] == shortest[s]) {
-                std::cout << '*';
+                if(s == 0) {
+                    current->at(0) = 1;
+                }
+                else {
+                    current->at(s) = previous->at(s-1) + 1;
+                }
+                std::cout << "* ";
             }
             else {
-                std::cout << '-';
+                // store if size is large enough?
+                std::cout << "- ";
+                current->at(s) = 0;
             }
         }
-        std::cout << "\n";
+        std::cout << "\n   | ";
+        for(s = 0; s < size_s; s++) {
+            printf("%-2u", current->at(s));
+        }
+        
+        std::swap(previous, current);
+        std::cout << "\n\n";
     }
 }
 
