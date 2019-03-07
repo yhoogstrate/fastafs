93c43bda8070310dfdd1ba17c0d4ebedf7ec286c
yhoogstrate <y.hoogstrate@gmail.com>
2019-03-07 15:55:06 +0100

    add tmpfile

---
+++ include/ucsc2bit_to_fastafs.hpp
@@ -0,0 +1,56 @@
+
+#include <vector>
+
+#include <openssl/sha.h>
+
+#include "config.hpp"
+#include "utils.hpp"
+
+#include "fastafs.hpp"
+#include "twobit_byte.hpp"
+
+//class ucsc2bit_to_fastafs_seq : public fastafs_seq
+//{
+//private:
+//twobit_byte twobit_data;
+
+//bool previous_was_N;
+
+//public:
+//std::vector<unsigned char> twobits;
+
+//unsigned int N;// effective size of unnknown nulceotides (N's) in nt
+
+//ucsc2bit_to_fastafs_seq(void);
+
+//size_t size(void);// size of compressed data, may be longer than 4*this->n
+
+//void add_N();
+//void add_nucleotide(unsigned char);
+//void add_twobit(twobit_byte &);
+//void flush_reading();
+//void close_reading();
+
+////void print(void);
+//SHA_CTX ctx;
+//unsigned char sha1_digest[SHA_DIGEST_LENGTH];
+//};
+
+
+
+//class ucsc2bit_to_fastafs
+//{
+//std::string name;
+//std::string filename;
+
+//std::vector<ucsc2bit_to_fastafs_seq *> data;
+
+//public:
+//ucsc2bit_to_fastafs(std::string, std::string);
+//~ucsc2bit_to_fastafs();
+//int cache(void);
+////void print();
+//void write(std::string);
+//unsigned int get_index_size();
+//unsigned int get_sequence_offset(unsigned int);
+//};
---
+++ src/ucsc2bit_to_fastafs.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+
+#include "config.hpp"
+
+#include "ucsc2bit_to_fastafs.hpp"
+#include "utils.hpp"
+
