f34706ffdfab5df3c793784cefa7fea63f61e10c
yhoogstrate <y.hoogstrate@gmail.com>
2019-04-02 11:42:38 +0200

    reference for testing close to completion

--- test/cache/test_cache.cpp
+++ test/cache/test_cache.cpp
@@ -208,86 +208,93 @@ BOOST_AUTO_TEST_CASE(test_cache)
     
     static std::string reference = 
         // GENERIC-HEADER
-        FASTAFS_MAGIC + FASTAFS_VERSION + "\x00\x00\x00\x01"s "\x00\x00\x00\x00"s // 4
+        FASTAFS_MAGIC + //        [0, 3] 
+        FASTAFS_VERSION + //      [4, 7] 
+        "\x00\x00\x00\x01"s//     [8, 11] still version 0.0.0.1
+        "\x00\x00\x01\x1D"s //    [12, 15] index position in file (285)
         
         // DATA
-        "\x00\x00\x00\x10"s// seq length [16] (of 2bit encoded bytes; n-blocks are excluded)
-        "\x00\x55\xAA\xFF"s// sequence
-        "\x00\x00\x00\x00"s// n-blocks
-        "00000000000000000000"s// checksum
-        "\x00\x00\x00\x00"s// m-blocks
+        "\x00\x00\x00\x10"s//     [12+4, 12+7] seq length (16) (of 2bit encoded bytes; n-blocks are excluded)
+        "\x00\x55\xAA\xFF"s//     [12+8, 12+11] sequence
+        "\x00\x00\x00\x00"s//     [12+12, 12+15] n-blocks (0)
+        "????????????????????"s// [12+16, 12+35] checksum
+        "\x00\x00\x00\x00"s//     [12+36, 12+39] m-blocks (0)
         
-        "\x10\x00\x00\x00"s// seq length (of 2bit encoded bytes; n-blocks are excluded)
-        "\x93\x93\x00\x93"s// sequence: ACTG ACTG nnnn ACTG = 10010011 10010011 00000000 10010011 = \x93 \x93 \x00 \x93
-        "\x01\x00\x00\x00"s// n-blocks (1)
-        "\x08\x00\x00\x00"s// n-block start[1]
-        "\x04\x00\x00\x00"s// n-block ends[1]
-        "00000000000000000000"s// checksum
-        "\x00\x00\x00\x00"s// m-blocks
+        "\x00\x00\x00\x0C"s//     [12+40, 12+43] seq length (12) (of 2bit encoded bytes; n-blocks are excluded)
+        "\x93\x93\x93"s//         [12+44, 12+46] sequence: ACTG ACTG nnnn ACTG = 10010011 10010011 00000000 10010011 = \x93 \x93 \x00 \x93
+        "\x00\x00\x00\x01"s//     [12+47, 12+50] n-blocks (1)
+        "\x00\x00\x00\x08"s//     [12+51, 12+54] n-block start[1] (08)
+        "\x00\x00\x00\x0B"s//     [12+55, 12+58] n-block ends[1] (11)
+        "????????????????????"s// [12+59, 12+78] checksum
+        "\x00\x00\x00\x00"s//     [12+79, 12+82] m-blocks (0)
         
-        "\x0D\x00\x00\x00"s// seq length (needs to become 2bit-encoded seq-len)
-        "\x93\x93\xAA\x40"s// sequence: last one is 01 00 00 00
-        "\x00\x00\x00\x00"s// n-blocks (0)
-        "00000000000000000000"s// checksum
-        "\x00\x00\x00\x00"s// m-blocks (0)
+        "\x00\x00\x00\x0D"s//     [12+83, 12+86] seq length (13) (needs to become 2bit-encoded seq-len)
+        "\x93\x93\xAA\x40"s//     [12+87, 12+90] sequence: last one is 01 00 00 00
+        "\x00\x00\x00\x00"s//     [12+91, 12+94] n-blocks (0)
+        "????????????????????"s// [12+95, 12+114] checksum
+        "\x00\x00\x00\x00"s//     [12+115, 12+118] m-blocks (0)
         
-        "\x0E\x00\x00\x00"s// seq length (of 2bit encoded bytes; n-blocks are excluded)
-        "\x93\x93\xAA\x50"s// last one is 01 01 00 00
-        "\x00\x00\x00\x00"s// n-bocks (0)
-        "00000000000000000000"s// checksum
-        "\x00\x00\x00\x00"s// m-blocks (0)
+        "\x00\x00\x00\x0E"s// [12+119, 12+122] seq length (14) (of 2bit encoded bytes; n-blocks are excluded)
+        "\x93\x93\xAA\x50"s// [12+123, 12+126] last one is 01 01 00 00
+        "\x00\x00\x00\x00"s// [12+127, 12+130] n-bocks (0)
+        "????????????????????"s// [12+131, 12+150] checksum
+        "\x00\x00\x00\x00"s// [12+151, 12+154] m-blocks (0)
         
-        "\x0F\x00\x00\x00"s// seq length (of 2bit encoded nucleotides; n-blocks are excluded)
-        "\x93\x93\xAA\x54"s// last one is 01 01 01 00
-        "\x00\x00\x00\x00"s// n-blocks (0)
-        "00000000000000000000"s// checksum
-        "\x00\x00\x00\x00"s// m-blocks (0)
+        "\x00\x00\x00\x0F"s// [12+155, 12+158] seq length (15) (of 2bit encoded nucleotides; n-blocks are excluded)
+        "\x93\x93\xAA\x54"s// [12+159, 12+162] last one is 01 01 01 00
+        "\x00\x00\x00\x00"s// [12+163, 12+166] n-blocks (0)
+        "????????????????????"s// [12+167, 12+186] checksum
+        "\x00\x00\x00\x00"s// [12+187, 12+190] m-blocks (0)
         
-        "\x08\x00\x00\x00"s// seq length (of 2bit encoded nucleotides; n-blocks are excluded)
-        "\x93\x00"s// sequence: ACTG NNNN = 10010011 00000000
-        "\x01\x00\x00\x00"s// n-blocks (1)
-        "\x04\x00\x00\x00"s// n-starts [1]
-        "\x04\x00\x00\x00"s// n-ends [1]
-        "00000000000000000000"s// checksum
-        "\x00\x00\x00\x00"s// m-blocks (0)
+        "\x00\x00\x00\x04"s// [12+191, 12+194] seq length (4) (of 2bit encoded nucleotides; n-blocks are excluded)
+        "\x93"s//               [12+195, 12+195] sequence: ACTG NNNN = 10010011 00000000
+        "\x00\x00\x00\x01"s// [12+196, 12+199] n-blocks (1)
+        "\x00\x00\x00\x04"s// [12+200, 12+203] n-starts [1] (4)
+        "\x00\x00\x00\x07"s// [12+204, 12+207] n-ends [1] (7)
+        "????????????????????"s// [12+208, 12+227] checksum
+        "\x00\x00\x00\x00"s//   [12+228, 12+231] m-blocks (0)
         
-        "\x06\x00\x00\x00"s// seq length (of 2bit encoded nucleotides; n-blocks are excluded)
-        "\x09\x30"s// sequence: NNAC TG?? = 00001001 00110000
-        "\x01\x00\x00\x00"s// n-blocks (1)
-        "\x00\x00\x00\x00"s// n-starts[1]
-        "\x02\x00\x00\x00"s// n-ends[1]
-        "00000000000000000000"s// checksum
-        "\x00\x00\x00\x00"s// m-blocks
+        "\x00\x00\x00\x04"s// [12+232, 12+235] seq length (4) (of 2bit encoded nucleotides; n-blocks are excluded)
+        "\x09"s// [12+236, 12+236] sequence: NNAC TG?? = 00001001 00110000
+        "\x00\x00\x00\x01"s// [12+237, 12+240] n-blocks (1)
+        "\x00\x00\x00\x00"s// [12+241, 12+244] n-starts[1] (0)
+        "\x00\x00\x00\x01"s// [12+245, 12+248] n-ends[1] (1)
+        "????????????????????"s// [12+249, 12+268] checksum
+        "\x00\x00\x00\x00"s// [12+269, 12+272] m-blocks (0)
 
-        "\x00\x00\x00\x07"s     // 7 sequences
+    // INDEX
+        "\x00\x00\x00\x07"s     // [12+273=285, 12+276] 7 sequences
         
-        "\x08"                  // complete, DNA and not circular
-        "\x04"s "chr1"s
-        "\x55\x00\x00\x00"s
+        "\x08"                  // [, ] complete, DNA and not circular
+        "\x04"s "chr1"s         // [, ] name
+        "\x55\x00\x00\x00"s     // [, ] data position in file (12+4)
         
-        "\x08"                  // complete, DNA and not circular
-        "\x04"s "chr2"s
-        "\x69\x00\x00\x00"s
+        "\x08"                  // [, ] complete, DNA and not circular
+        "\x04"s "chr2"s         // [, ] name
+        "\x69\x00\x00\x00"s     // [, ] data position in file (12+40)
         
-        "\x08"                  // complete, DNA and not circular
-        "\x06"s "chr3.1"s
-        "\x85\x00\x00\x00"s
+        "\x08"                  // [, ] complete, DNA and not circular
+        "\x06"s "chr3.1"s       // [, ] name
+        "\x85\x00\x00\x00"s     // [, ] data position in file (12+83)
         
-        "\x08"                  // complete, DNA and not circular
-        "\x06"s "chr3.2"s
-        "\x99\x00\x00\x00"s
+        "\x08"                  // [, ] complete, DNA and not circular
+        "\x06"s "chr3.2"s       // [, ] name
+        "\x99\x00\x00\x00"s     // [, ] data position in file (12+119)
         
-        "\x08"                  // complete, DNA and not circular
-        "\x06"s "chr3.3"s
-        "\xAD\x00\x00\x00"s
+        "\x08"                  // [, ] complete, DNA and not circular
+        "\x06"s "chr3.3"s       // [, ] name
+        "\xAD\x00\x00\x00"s     // [, ] data position in file (12+155)
         
-        "\x08"                  // complete, DNA and not circular
-        "\x04"s "chr4"s
-        "\xC1\x00\x00\x00"s
+        "\x08"                  // [, ] complete, DNA and not circular
+        "\x04"s "chr4"s         // [, ] name
+        "\xC1\x00\x00\x00"s     // [, ] data position in file (12+191)
         
-        "\x08"                  // complete, DNA and not circular
-        "\x04"s "chr5"s
-        "\xDB\x00\x00\x00"s
+        "\x08"                  // [, ] complete, DNA and not circular
+        "\x04"s "chr5"s         // [, ] name
+        "\xDB\x00\x00\x00"s     // [, ] data position in file (12+232)
+    
+    // METADATA
+        "\x00"                  // no metadata fields [padding will come soon?]
     ;
     //BOOST_CHECK(output.compare(uppercase) == 0 or output.compare(mixedcase) == 0);
 }
