7d21daae34320121ebea463eca00b8add42ce056
yhoogstrate <y.hoogstrate@gmail.com>
2019-05-21 12:34:48 +0200

    relicense to allow writing a kernel module

--- src/main.cpp
+++ src/main.cpp
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
         } else if(strcmp(argv[1], "--version") == 0) {
             std::cout << PACKAGE << " v" << PACKAGE_VERSION << GIT_SHA1_STRING << "\n\n";
             std::cout << "Copyright (C) 2017 Youri Hoogstrate." << "\n";
-            std::cout << "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n";
+            std::cout << "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>.\n";
             std::cout << "This is free software: you are free to change and redistribute it.\n";
             std::cout << "There is NO WARRANTY, to the extent permitted by law.\n\n";
             std::cout << "The " << PACKAGE << " package is written by Youri Hoogstrate.\n";
