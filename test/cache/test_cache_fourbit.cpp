b499fc64c079b460e4c4157f7ca49136e390fcae
yhoogstrate <y.hoogstrate@gmail.com>
2019-11-27 16:43:15 +0100

    firstr part of cache test succeeded

--- test/cache/test_cache_fourbit.cpp
+++ test/cache/test_cache_fourbit.cpp
@@ -55,132 +55,72 @@ BOOST_AUTO_TEST_CASE(test_equality_fourbit_byte)
     delete[] seq1;
     delete[] seq2;
     
-/*
-
-    // test 00 11 00 00 -> 00110000 -> 16+32 -> 48
-    b.set(6, NUCLEOTIDE_T);
-    b.set(4, NUCLEOTIDE_G);
-    b.set(2, NUCLEOTIDE_T);
-    b.set(0, NUCLEOTIDE_T);
-    BOOST_CHECK_EQUAL(b.data, 48);
+	// GT: 0010 0011
+    b.set(4, 2); // G 
+    b.set(0, 3); // T
+    BOOST_CHECK_EQUAL(b.data, 35);
 
     seq1 = b.get(1);
     seq2 = b.get(2);
-    seq3 = b.get(3);
-    seq4 = b.get(4);
-    seq = b.get();
-
-    BOOST_CHECK_EQUAL(strcmp(seq1, "T"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq2, "TG"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq3, "TGT"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq4, "TGTT"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq, "TGTT"), 0);
-
-    delete[] seq1;
-    delete[] seq2;
-    delete[] seq3;
-    delete[] seq4;
-
-
-    // test 11 00 00 00 -> 11000000 -> 64+128 -> 192
-    b.set(6, NUCLEOTIDE_G);
-    b.set(4, NUCLEOTIDE_T);
-    b.set(2, NUCLEOTIDE_T);
-    b.set(0, NUCLEOTIDE_T);
-    BOOST_CHECK_EQUAL(b.data, 192);
-
-    seq1 = b.get(1);
-    seq2 = b.get(2);
-    seq3 = b.get(3);
-    seq4 = b.get(4);
     seq = b.get();
 
     BOOST_CHECK_EQUAL(strcmp(seq1, "G"), 0);
     BOOST_CHECK_EQUAL(strcmp(seq2, "GT"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq3, "GTT"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq4, "GTTT"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq, "GTTT"), 0);
+    BOOST_CHECK_EQUAL(strcmp(seq, "GT"), 0);
 
     delete[] seq1;
     delete[] seq2;
-    delete[] seq3;
-    delete[] seq4;
 
 
-    // test 10 01 01 10 -> 10010110 -> 2 + 4 + 16 + 128 -> 150
-    b.set(6, NUCLEOTIDE_A);
-    b.set(4, NUCLEOTIDE_C);
-    b.set(2, NUCLEOTIDE_C);
-    b.set(0, NUCLEOTIDE_A);
-    BOOST_CHECK_EQUAL(b.data, 150);
+	// set to UR (0100 0101)
+    b.set(4, 4);
+    b.set(0, 5);
+    BOOST_CHECK_EQUAL(b.data, 69);
 
     seq1 = b.get(1);
     seq2 = b.get(2);
-    seq3 = b.get(3);
-    seq4 = b.get(4);
+    seq = b.get();
+
+    BOOST_CHECK_EQUAL(strcmp(seq1, "U"), 0);
+    BOOST_CHECK_EQUAL(strcmp(seq2, "UR"), 0);
+    BOOST_CHECK_EQUAL(strcmp(seq, "UR"), 0);
+
+    delete[] seq1;
+    delete[] seq2;
+
+
+    // set to AN (0000 1111)
+    b.set(4, 0);
+    b.set(0, 15);
+    BOOST_CHECK_EQUAL(b.data, 15);
+
+    seq1 = b.get(1);
+    seq2 = b.get(2);
     seq = b.get();
 
     BOOST_CHECK_EQUAL(strcmp(seq1, "A"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq2, "AC"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq3, "ACC"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq4, "ACCA"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq, "ACCA"), 0);
+    BOOST_CHECK_EQUAL(strcmp(seq2, "AN"), 0);
+    BOOST_CHECK_EQUAL(strcmp(seq, "AN"), 0);
 
     delete[] seq1;
     delete[] seq2;
-    delete[] seq3;
-    delete[] seq4;
 
 
-    // test 11 11 11 11 -> 11111111 -> 255
-    b.set(6, NUCLEOTIDE_G);
-    b.set(4, NUCLEOTIDE_G);
-    b.set(2, NUCLEOTIDE_G);
-    b.set(0, NUCLEOTIDE_G);
-    BOOST_CHECK_EQUAL(b.data, 255);
+    // set to NA (1111 0000)
+    b.set(4, 15);
+    b.set(0, 0);
+    BOOST_CHECK_EQUAL(b.data, 240);
 
     seq1 = b.get(1);
     seq2 = b.get(2);
-    seq3 = b.get(3);
-    seq4 = b.get(4);
     seq = b.get();
 
-    BOOST_CHECK_EQUAL(strcmp(seq1, "G"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq2, "GG"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq3, "GGG"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq4, "GGGG"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq, "GGGG"), 0);
+    BOOST_CHECK_EQUAL(strcmp(seq1, "N"), 0);
+    BOOST_CHECK_EQUAL(strcmp(seq2, "NA"), 0);
+    BOOST_CHECK_EQUAL(strcmp(seq, "NA"), 0);
 
     delete[] seq1;
     delete[] seq2;
-    delete[] seq3;
-    delete[] seq4;
-
-
-    // test 00 00 00 00 -> 00000000 -> 0
-    b.set(6, NUCLEOTIDE_T);
-    b.set(4, NUCLEOTIDE_T);
-    b.set(2, NUCLEOTIDE_T);
-    b.set(0, NUCLEOTIDE_T);
-    BOOST_CHECK_EQUAL(b.data, 0);
-
-    seq1 = b.get(1);
-    seq2 = b.get(2);
-    seq3 = b.get(3);
-    seq4 = b.get(4);
-    seq = b.get();
-
-    BOOST_CHECK_EQUAL(strcmp(seq1, "T"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq2, "TT"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq3, "TTT"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq4, "TTTT"), 0);
-    BOOST_CHECK_EQUAL(strcmp(seq, "TTTT"), 0);
-
-    delete[] seq1;
-    delete[] seq2;
-    delete[] seq3;
-    delete[] seq4;
-    */
 }
 
 
