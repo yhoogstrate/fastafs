c3f0812e156079e9f1a4d40f71db7bee80c2a58d
yhoogstrate <y.hoogstrate@gmail.com>
2020-01-20 21:20:06 +0100

    more tests done

--- test/sequenceregion/test_sequenceregion.cpp
+++ test/sequenceregion/test_sequenceregion.cpp
@@ -102,6 +102,83 @@ BOOST_AUTO_TEST_CASE(test_sequence_region)
 
 
 
+BOOST_AUTO_TEST_CASE(test_sequence_region3)
+{
+    {
+        char arg[] = "/seq/chrRr1";
+        SequenceRegion sr = SequenceRegion(&(arg[5]));
+        
+        BOOST_CHECK_EQUAL(sr.seq_name , "chrRr1");
+        BOOST_CHECK_EQUAL(sr.has_defined_end , false); // not defined; sequence's end
+    }
+
+    {
+        char arg[] = "/seq/chrRr1:";
+        SequenceRegion sr = SequenceRegion(&(arg[5]));
+
+        BOOST_CHECK_EQUAL(sr.seq_name , "chrRr1");
+        BOOST_CHECK_EQUAL(sr.has_defined_end , false); // not defined; sequence's end
+    }
+
+    {
+        char arg[] = "/seq/chrRr1:1234";
+        SequenceRegion sr = SequenceRegion(&(arg[5]));
+
+        BOOST_CHECK_EQUAL(sr.seq_name , "chrRr1");
+        BOOST_CHECK_EQUAL(sr.start , 1234);
+
+        BOOST_CHECK_EQUAL(sr.has_defined_end , true);
+        BOOST_CHECK_EQUAL(sr.end , 1234);
+    }
+
+    {
+        char arg[] = "/seq/chrRr1:-1234";
+        SequenceRegion sr = SequenceRegion(&(arg[5]));
+
+        BOOST_CHECK_EQUAL(sr.seq_name , "chrRr1");
+        BOOST_CHECK_EQUAL(sr.start , 0);
+
+        BOOST_CHECK_EQUAL(sr.has_defined_end , true);
+        BOOST_CHECK_EQUAL(sr.end , 1234);
+    }
+    
+    {
+        char arg[] = "/seq/chrRr1:1234-1235";
+        SequenceRegion sr = SequenceRegion(&(arg[5]));
+
+        BOOST_CHECK_EQUAL(sr.seq_name , "chrRr1");
+        BOOST_CHECK_EQUAL(sr.start , 1234);
+
+        BOOST_CHECK_EQUAL(sr.has_defined_end , true);
+        BOOST_CHECK_EQUAL(sr.end , 1235);
+    }
+
+
+    {
+        char arg[] = "/seq/chrRr1:1234-";
+        SequenceRegion sr = SequenceRegion(&(arg[5]));
+
+        BOOST_CHECK_EQUAL(sr.seq_name , "chrRr1");
+        BOOST_CHECK_EQUAL(sr.start , 1234);
+
+        BOOST_CHECK_EQUAL(sr.has_defined_end , false);
+        //BOOST_CHECK_EQUAL(sr.end , 1235); - underfined
+    }
+
+    {
+        char arg[] = "/seq/chrRr1:1235-1234";
+
+        SequenceRegion *sr = nullptr;
+        if(sr == nullptr) {// compiler doesn't understand this otherwise
+            BOOST_CHECK_THROW(sr = new SequenceRegion(&(arg[5])) , std::invalid_argument);
+        }
+    }
+
+}
+
+
+
+
 BOOST_AUTO_TEST_CASE(test_sequence_region2)
 {
     {
