fa2ef7f65ec1bd057f4a15589cd0bf694ab74a95
youri <y.hoogstrate@gmail.com>
2020-04-07 17:08:50 +0200

    sav

--- build-zstd.sh
+++ build-zstd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -o zstd-s-c seekable_compression.cpp zstdseek_compress.c -I"dependencies/zstd/lib/common" -I"dependencies/zstd/lib" "dependencies/zstd/lib/libzstd.a"
+g++ -o bin/zstd-s-c dependencies/zstd-seekable-adapted/seekable_compression.cpp dependencies/zstd-seekable-adapted/zstdseek_compress.c -I"dependencies/zstd/lib/common" -I"dependencies/zstd/lib" "dependencies/zstd/lib/libzstd.a"
