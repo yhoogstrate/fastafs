fcd5857acacd481017fb89b0448b3ff4da4c44cb
yhoogstrate <y.hoogstrate@gmail.com>
2026-04-17 09:33:19 +0200

    byte_Decoder_optims

--- include/byte_decoder.hpp
+++ include/byte_decoder.hpp
@@ -3,122 +3,94 @@
 #define BYTE_DECODER_HPP
 
 #include "config.hpp"
+#include "twobit_byte.hpp"
+#include "fourbit_byte.hpp"
+#include "fivebit_fivebytes.hpp"
 
-#include <array>
-#include <iostream>
-#include <fstream>
-
-#include <algorithm>
-#include <functional>
 #include <memory>
+#include <string>
+#include <cstring>
 
 
 
-// https://refactoring.guru/design-patterns/state/cpp/example
-// https://refactoring.guru/design-patterns/strategy/cpp/example [looks more like strategy]
-
-
-class byte_decoder_interface // generic decoder interface/
+class byte_decoder_interface
 {
-private:
-    unsigned int size_block_encoded; //
-    unsigned int size_block_decoded; //
-
-    int block_size_1;
-
 public:
     virtual ~byte_decoder_interface() = default;
-    virtual std::string doAlgorithm(const unsigned char* data) const = 0;
+    virtual unsigned char bytes_per_chunk() const = 0;
+    virtual std::string doAlgorithm(const unsigned char *data) const = 0;
 };
-
 
 
 
 class byte_decoder
 {
 private:
-    size_t buffer_input_size; // set to 0
-    //unsigned char buffer_input_data[1024]; // bytes of raw decoded data
-    const unsigned char *buffer_input_data;
+    const unsigned char *buffer_input_data = nullptr;
+    size_t buffer_input_size = 0;
 
-    size_t buffer_decoded_size; // set to 0
-    unsigned char buffer_decoded_data[1024]; // bytes of decoded data
-    unsigned char *buffer_decoded_data_i;
-
-
-    std::unique_ptr<byte_decoder_interface> byte_decoder_interface_;
+    std::unique_ptr<byte_decoder_interface> strategy_;
 
 public:
     explicit byte_decoder(std::unique_ptr<byte_decoder_interface> &&strategy);
     void set_strategy(std::unique_ptr<byte_decoder_interface> &&strategy);
-
     void set_input_data(const unsigned char *input_data, size_t input_data_size);
-
-    void decode();
-
+    std::string decode() const;
 };
 
 
 
-// four bit byte etc: 
-class byte_decoder_interface_A : public byte_decoder_interface
+class byte_decoder_interface_twobit_dna : public byte_decoder_interface
 {
 public:
-    std::string doAlgorithm(const unsigned char* data) const override 
+    unsigned char bytes_per_chunk() const override { return 1; }
+    std::string doAlgorithm(const unsigned char *data) const override
     {
-            std::string out = "";
-            
-            out += " [4b]";
-
-    return out;
+        return std::string(ENCODE_HASH_TWOBIT_DNA[data[0]], 4);
     }
 };
 
 
-/*
- * two bit byte etc:
- * "AAAA" => "\x10 \x10 \x10 \x10"
- * "CAAA" => "\x01 \x10 \x10 \x10"
- * "TAAA" => "\x00 \x10 \x10 \x10"
- * "GAAA" => "\x11 \x10 \x10 \x10"
- */
-class byte_decoder_interface_B : public byte_decoder_interface
+
+class byte_decoder_interface_twobit_rna : public byte_decoder_interface
 {
 public:
-    std::string doAlgorithm(const unsigned char* data) const override
+    unsigned char bytes_per_chunk() const override { return 1; }
+    std::string doAlgorithm(const unsigned char *data) const override
     {
-        const unsigned char hash_size_input = 4;
-        const unsigned char hash_size_output = 1;
-        
-        std::string out = "";
-        
-        for(int i = 0; i < 5; i++) {
-            out += data[i];
-        }
-        
-        out += " [2b]";
-
-        return out;
+        return std::string(ENCODE_HASH_TWOBIT_RNA[data[0]], 4);
     }
 };
 
-// five bit byte etc
-class byte_decoder_interface_C : public byte_decoder_interface
+
+
+class byte_decoder_interface_fourbit : public byte_decoder_interface
 {
 public:
-    std::string doAlgorithm(const unsigned char* data) const override
+    unsigned char bytes_per_chunk() const override { return 1; }
+    std::string doAlgorithm(const unsigned char *data) const override
     {
-            std::string out = "";
-            
-            out += " [5b]";
+        return std::string(fourbit_byte::encode_hash[data[0]], 2);
+    }
+};
 
-            return out;
+
+
+class byte_decoder_interface_fivebit : public byte_decoder_interface
+{
+public:
+    unsigned char bytes_per_chunk() const override { return 5; }
+    std::string doAlgorithm(const unsigned char *data) const override
+    {
+        fivebit_fivebytes chunk;
+        unsigned char compressed[5] = {data[0], data[1], data[2], data[3], data[4]};
+        chunk.set_compressed(compressed);
+        return std::string(reinterpret_cast<const char *>(chunk.data_decompressed), 8);
     }
 };
 
 
 
 #endif
-
 
 
--- src/byte_decoder.cpp
+++ src/byte_decoder.cpp
@@ -1,46 +1,38 @@
 
 
-#include <iostream>
-#include <fstream>
+#include <string>
 
 #include "config.hpp"
-
 #include "byte_decoder.hpp"
 
 
 
-
-byte_decoder::byte_decoder(std::unique_ptr<byte_decoder_interface> &&strategy = {}) : byte_decoder_interface_(std::move(strategy))
+byte_decoder::byte_decoder(std::unique_ptr<byte_decoder_interface> &&strategy) : strategy_(std::move(strategy))
 {
-};
+}
 
 
 void byte_decoder::set_strategy(std::unique_ptr<byte_decoder_interface> &&strategy)
 {
-    byte_decoder_interface_ = std::move(strategy);
-};
-
+    strategy_ = std::move(strategy);
+}
 
 
 void byte_decoder::set_input_data(const unsigned char *input_data, size_t input_data_size)
 {
-    this->buffer_input_size = input_data_size;
-    this->buffer_input_data = input_data;
+    buffer_input_data = input_data;
+    buffer_input_size = input_data_size;
 }
 
 
-
-void byte_decoder::decode()
+std::string byte_decoder::decode() const
 {
-    printf("decoding ... to be implemented by interface\n");
-    std::string test("aaa");
-    printf(this->byte_decoder_interface_->doAlgorithm(this->buffer_input_data).c_str());
-    printf("\n");
+    std::string result;
+    const unsigned char chunk_size = strategy_->bytes_per_chunk();
+    for(size_t i = 0; i + chunk_size <= buffer_input_size; i += chunk_size) {
+        result += strategy_->doAlgorithm(buffer_input_data + i);
+    }
+    return result;
 }
-
-
-
-
-
 
 
